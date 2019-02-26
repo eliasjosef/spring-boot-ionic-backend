c1879d00cdd569920a9f00a80ce6433f5aa7cae0
Elias Santos <elias.j.s.f@gmail.com>
2019-02-26 10:47:47 -0300

    Projeto Criado

---
+++ src/main/java/com/elias/regaliacrud/RegaliacrudApplication.java
@@ -0,0 +1,13 @@
+package com.elias.regaliacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegaliacrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegaliacrudApplication.class, args);
+	}
+
+}
