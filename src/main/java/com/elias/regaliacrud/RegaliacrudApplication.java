944d2c3d444b32a2a314d88dc48ebd9858313727
Elias Santos <elias.j.s.f@gmail.com>
2019-02-26 13:33:42 -0300

    Criando operação de instanciação

--- src/main/java/com/elias/regaliacrud/RegaliacrudApplication.java
+++ src/main/java/com/elias/regaliacrud/RegaliacrudApplication.java
@@ -1,14 +1,32 @@
 package com.elias.regaliacrud;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RegaliacrudApplication {
+import com.elias.regaliacrud.domain.Pessoa;
+import com.elias.regaliacrud.repositories.PessoaRepository;
 
+@SpringBootApplication
+public class RegaliacrudApplication implements CommandLineRunner {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RegaliacrudApplication.class, args);
 			
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Pessoa p1 = new Pessoa(null, "Maria");
+		Pessoa p2 = new Pessoa(null, "Lucia");
+		
+		pessoaRepository.saveAll(Arrays.asList(p1, p2));
 	}
 
 }
